f0d93e52ed1d08af127d6b85310de533d5733936
nort9716-dot <nort9716@gmail.com>
2026-09-04 21:37:42 +0330

    Bump PWA cache for creative production workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='character-studio-v3';
+const CACHE='character-studio-v4';
 const APP=['./','./index.html','./pwa.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
